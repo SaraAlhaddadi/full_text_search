4af52c9feee6bf15c513621bd608ce03faabe7f2
Sara Alhaddadi <sara.khalid.alhaddadi@gmail.com>
2023-10-20 23:52:36 +0300

    add full_text search to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,14 @@
 class Job < ApplicationRecord
+  include PgSearch::Model
+  # Job.search_title('Pharmacist')
+  pg_search_scope :search_title, against: :title
+
+  # Job.search_job('Pharmacist Education')
+  pg_search_scope :search_job,
+                  against: %i[title sector skill],
+                  using: { tsearch: { dictionary: 'english' } }
+
+  pg_search_scope :search_job_score,
+                  against: { title: 'A', skill: 'B', sector: 'C' },
+                  using: { tsearch: { dictionary: 'english' } }
 end
